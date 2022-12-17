c243ed26bf67d0a7ad5d536ee8086b168df1e2f0
Aleksandr <a.v.agafonov@mail.ru>
2022-12-17 22:13:31 +0300

    Homework1

---
+++ Task4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine ("Введите число:  ");
+int a = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= a; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write(i + " ");
+    }
+    Console.ReadLine(    
+    );
+}  
+
