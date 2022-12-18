8730e7997fc1cea4d6470339669de5d6348f1aa0
Aleksandr <a.v.agafonov@mail.ru>
2022-12-18 20:04:41 +0300

    Homework1

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,17 +1,4 @@
-﻿// //Задание 3 
-// Console.WriteLine ("Введите число  ");
-// int a = int.Parse(Console.ReadLine());
-
-// if (a % 2 == 0)
-// {
-//   Console.WriteLine ($"Число {a} четное");
-// }  
-// else
-// {
-//  Console.WriteLine ($"Число {a} нечетное");   
-// }
-
-//Задание 4
+﻿//Задание 4
 Console.WriteLine ("Введите число:  ");
 int a = int.Parse(Console.ReadLine()!);
 
