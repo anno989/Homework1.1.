9fb15c9e522a907dc86d69b42ab552a2c466f004
Aleksandr <a.v.agafonov@mail.ru>
2022-12-18 21:20:39 +0300

    Homework1.1

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,40 +1,40 @@
-﻿// //Задание 4
+﻿//Задание 4 (вариант решения 1 - без отрицательных чисел)
+ Console.WriteLine ("Введите число:  ");
+ int a = int.Parse(Console.ReadLine()!);
+ while (a <= 0)
+ {
+     Console.WriteLine("Введите положительное число ");
+     a = int.Parse(Console.ReadLine()!);
+ }
+ for (int i = 1; i <= a; i++)
+ {
+     if (i % 2 == 0)
+     {
+         Console.Write(i + " ");
+     }
+}
+
+// //Задание 4 (вариант решения 2 - c отрицательными числами)  
 // Console.WriteLine ("Введите число:  ");
 // int a = int.Parse(Console.ReadLine()!);
-// while (a <= 0)
+// if (a <= 0)
 // {
-//     Console.WriteLine("Введите положительное число ");
-//     a = int.Parse(Console.ReadLine()!);
-// }
-// for (int i = 1; i <= a; i++)
-// {
-//     if (i % 2 == 0)
+//     for (int i = a; i <= 1; i++)
 //     {
-//         Console.Write(i + " ");
+//         if (i % 2 == 0)
+//         {
+//             Console.Write(i + " ");
+//         }
+//     }   
+// }
+// else 
+// {
+//     for (int i = 1; i <= a; i++)
+//     {
+//         if (i % 2 == 0)
+//         {
+//             Console.Write(i + " ");
+//         }
 //     }
 // }
-
-  
-Console.WriteLine ("Введите число:  ");
-int a = int.Parse(Console.ReadLine()!);
-if (a <= 0)
-{
-    for (int i = a; i <= 1; i++)
-    {
-        if (i % 2 == 0)
-        {
-            Console.Write(i + " ");
-        }
-    }   
-}
-else 
-{
-    for (int i = 1; i <= a; i++)
-    {
-        if (i % 2 == 0)
-        {
-            Console.Write(i + " ");
-        }
-    }
-}
 
