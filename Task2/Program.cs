676f4bc940313e614d2a70621d62dacc14e21117
Aleksandr <a.v.agafonov@mail.ru>
2022-12-18 20:03:18 +0300

    Homework1

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,26 +1,4 @@
-﻿// // Задание 1
-// Console.WriteLine ("Введите первое число  ");
-// int a = int.Parse(Console.ReadLine()!);
-// Console.WriteLine("Введите второе число ");
-// int b = int.Parse(Console.ReadLine()!);
-
-// if (a > b)
-// {
-//   Console.WriteLine ("max =" + a);
-//    Console.WriteLine ("min =" + b);
-// }
-// else if (a == b)
-// {
-//   Console.WriteLine ("max = min = " +a);
-// }  
-// else
-// {
-//  Console.WriteLine ("max =" + b);
-//    Console.WriteLine ("min =" + a);   
-// }
-
-
-// Задание 2
+﻿// Задание 2
 Console.WriteLine ("Введите первое число  ");
 int a = int.Parse(Console.ReadLine()!);
 Console.WriteLine("Введите второе число ");
---
+++ Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿ //Задание 3 
+ Console.WriteLine ("Введите число  ");
+ int a = int.Parse(Console.ReadLine());
+
+ if (a % 2 == 0)
+ {
+   Console.WriteLine ($"Число {a} четное");
+ }  
+ else
+ {
+  Console.WriteLine ($"Число {a} нечетное");   
+}
